d1d81f80d822e548011ea8b2b0dedbca8e55c09b
caioadauri <caioadaurigomes@gmail.com>
2025-03-26 08:27:29 -0300

    create function activate user

--- src/Application/Controllers/user_controller.py
+++ src/Application/Controllers/user_controller.py
@@ -1,28 +1,7 @@
-from flask import request, jsonify, make_response, render_template
+from flask import request, jsonify, make_response
 from Application.Service.user_service import UserService
 
 class UserController:
-    @staticmethod
-    def register_user():
-        if request.method == 'POST':
-            nome = request.form.get('nome')
-            cnpj = request.form.get('cnpj')
-            email = request.form.get('email')
-            celular = request.form.get('celular')
-            senha = request.form.get('senha')
-
-            if not nome or not cnpj or not email or not celular or not senha:
-                return make_response(jsonify({"erro": "Todos os campos são obrigatórios"}), 400)
-
-            user = UserService.create_user(nome, cnpj, email, celular, senha)
-
-            return make_response(jsonify({
-                "mensagem": "Cadastro realizado com sucesso",
-                "usuario": user.to_dict()
-            }), 200)
-
-        return render_template('cadastro.html')
-
     @staticmethod
     def register_userJson():
         data = request.get_json()
@@ -38,14 +17,24 @@ class UserController:
         user = UserService.create_user(nome, cnpj, email, celular, senha)
         
         return make_response(jsonify({
-            "mensagem": "User salvo com sucesso",
-            "usuarios": user.to_dict()
+            "mensagem": "Usuário salvo com sucesso. Código de ativação enviado via WhatsApp.",
+            "usuario": user.to_dict()
         }), 200)
 
 
     @staticmethod
     def activate_user():
-        pass
+        data = request.get_json()
+        email = data.get('email')
+        code = data.get('code')
+
+        if not email or not code:
+            return make_response(jsonify({"erro": "Email e código de ativação são obrigatórios"}), 400)
+        
+        result = UserService.activate_user(email, code)
+        if result:
+            return make_response(jsonify({"mensagem": "Conta ativada com sucesso"}), 200)
+        return make_response(jsonify({"erro": "Código inválido ou conta não encontrada"}), 400)
 
 
     @staticmethod
